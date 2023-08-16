9a114986bf574b366b1f12e7ac077c312b8058ee
Aggasth <101593156+Aggasth@users.noreply.github.com>
2023-08-16 12:37:59 -0400

    Create app.java

---
+++ app.java
@@ -0,0 +1,11 @@
+public class App {
+    public static void main(String[] args) {
+        String nombre = "Socius Partner";
+        int edad = 36;
+        String correo = "partner@socius.cl";
+        
+        System.out.println("Nombre: " + nombre);
+        System.out.println("Edad: " + edad);
+        System.out.println("Correo: " + correo);
+    }
+}
